b7f899108e0f28dcc8e18281ce33518280958a3a
Andrew DeYoung <axdeyoung@gmail.com>
2024-09-24 14:42:57 -0700

    Added parsing for Commander and Cargo events. Untested.

--- journalagent/journalparse/journalparse.go
+++ journalagent/journalparse/journalparse.go
@@ -2,32 +2,32 @@ package journalparse
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/axdeyoung/ed-wing-mission-helper/journalagent/config"
 )
 
-type genericEvent struct {
-	EventType string `json:"event"`
-}
-
 var dir string
+var commanderName string
 
 var (
-	journalReadSig      = make(chan struct{})
-	journalQuitSig      = make(chan struct{})
-	journalNewLinesChan = make(chan []string)
-	isJournalOpen       bool
+	journalReadSig        = make(chan struct{})
+	journalQuitSig        = make(chan struct{})
+	journalNewLinesChan   = make(chan []string)
+	journalListeningMutex sync.Mutex
 
-	cargoReadSig      = make(chan struct{})
-	cargoQuitSig      = make(chan struct{})
-	cargoNewLinesChan = make(chan []string)
-	isCargoOpen       bool
+	cargoReadSig        = make(chan struct{})
+	cargoQuitSig        = make(chan struct{})
+	cargoNewLinesChan   = make(chan []string)
+	cargoListeningMutex sync.Mutex
 )
 
 func InitJournalParse() {
@@ -45,27 +45,31 @@ func UpdateDir(newDir string) {
 	journalPath, err := newestJournalFilePath()
 	if err != nil {
 		fmt.Println("Unable to find newest Journal file path: ", err)
+		// If there is no journal file for any reason, ignore the error
+		// listenToFile will quietly fail and terminate. This is fine.
 	}
 
-	if isJournalOpen {
-		journalQuitSig <- struct{}{}
+	select {
+	case journalQuitSig <- struct{}{}:
 		// if there is journal file already open, close it
+	default:
 	}
-	isJournalOpen = true
-	go listenToFile(journalPath, true, journalReadSig, journalQuitSig, journalNewLinesChan)
+	go listenToFile(journalPath, true, journalReadSig, journalQuitSig, journalNewLinesChan, &journalListeningMutex)
 
-	if isCargoOpen {
-		cargoQuitSig <- struct{}{}
+	select {
+	case cargoQuitSig <- struct{}{}:
+		// if there is cargo file already open, close it
+	default:
 	}
-	isCargoOpen = true
-	go listenToFile("Cargo.json", false, cargoReadSig, cargoQuitSig, cargoNewLinesChan)
-
+	go listenToFile(journalFile("Cargo.json"), false, cargoReadSig, cargoQuitSig, cargoNewLinesChan, &cargoListeningMutex)
+	fmt.Println("Opened Journal Path: ", journalPath)
 	// TODO: notify user if any files fail to open. This may be the result of the game never having been played, an incorrect directory, or a non-existent directory.
 }
 
 func newestJournalFilePath() (string, error) {
 	var newestFilePath string
 	var newestTime time.Time
+	newestTime = time.Unix(0, 0)
 
 	fileRegex, err := regexp.Compile(`^Journal\.(\d{4}-\d{2}-\d{2}T\d{2}\d{2}\d{2})\.01\.log$`)
 	if err != nil {
@@ -106,9 +110,11 @@ func newestJournalFilePath() (string, error) {
 		return nil
 	})
 	if err != nil {
+		fmt.Println("Error finding journal file: ", err)
 		return "", fmt.Errorf("error walking directory %s: %w", dir, err)
 	}
 
+	fmt.Println("Found journal path: ", newestFilePath)
 	return newestFilePath, nil
 }
 
@@ -116,12 +122,16 @@ func GetDir() string {
 	return dir
 }
 
+func GetCommanderName() string {
+	return commanderName
+}
 func journalFile(fileName string) string {
 	return filepath.Join(dir, fileName)
 }
 
 func UserDataJson() string {
-	return `{"data": "This is where the user data will go"}`
+	// structure is so simple it should be fine to just marshal it manually.
+	return fmt.Sprintf(`{ "Name":"%s" }`, commanderName)
 }
 
 func listenToFile(
@@ -129,11 +139,18 @@ func listenToFile(
 	isLog bool,
 	readSigChan <-chan struct{},
 	quitSigChan <-chan struct{},
-	newLinesChan chan<- []string) error {
+	newLinesChan chan<- []string,
+	fileMutex *sync.Mutex) error {
+
+	fileMutex.Lock()
+	defer fileMutex.Unlock()
+
 	file, err := os.Open(filePath)
 	if err != nil {
 		return err
 	}
+	fmt.Println("Opened file and started listening: ", filePath)
+	defer fmt.Println("Stopped listening and closed file: ", filePath)
 	defer file.Close()
 	for {
 		select {
@@ -154,4 +171,86 @@ func listenToFile(
 			return nil
 		}
 	}
+}
+
+func GetUpdatesJson() (string, error) {
+	cargoToChange := false
+	updatesToSend := ""
+
+	select {
+	case journalReadSig <- struct{}{}:
+	default:
+		return "", fmt.Errorf("error reading journal file")
+	}
+	latestJournalEntriesSliceRaw := <-journalNewLinesChan
+
+	for _, rawEntry := range latestJournalEntriesSliceRaw {
+		var entry map[string]any
+		err := json.Unmarshal([]byte(rawEntry), &entry)
+		if err != nil {
+			fmt.Println("Warning: error unmarshalling journal entry: ", rawEntry)
+			continue
+		}
+
+		newUpdateToSend, cargoChanged, err := filterAndParseJournalEntry(entry)
+		if err != nil {
+			fmt.Println("Warning: error parsing journal entry: ", rawEntry, ": ", err)
+			continue
+		}
+		if newUpdateToSend != "" {
+			updatesToSend += newUpdateToSend + "\n"
+		}
+		if cargoChanged {
+			cargoToChange = true
+		}
+	}
+	if cargoToChange {
+		newCargoToSend, err := parseCargo()
+		if err != nil {
+			return "", fmt.Errorf("unable to parse cargo: %w", err)
+		}
+		updatesToSend += newCargoToSend + "\n"
+	}
+	return updatesToSend, nil
+}
+
+/*
+returns 3 values:
+
+	string: a JSON entry that is to be appended to the data sent to the server. May be empty.
+	bool: Does the given entry indicate a cargo update?
+	error: any error causing this function to fail.
+*/
+func filterAndParseJournalEntry(entry map[string]any) (string, bool, error) {
+	var err error
+	var dataToSend []byte
+	var updateCargo bool
+
+	err = nil
+	updateCargo = false
+
+	switch entry["event"] {
+	case "Commander":
+		commanderName = entry["Name"].(string)
+		delete(entry, "FID")
+		dataToSend, err = json.Marshal(entry)
+	case "Cargo":
+		updateCargo = true
+	default: // not an event type we care about. Ignore it.
+		return "", false, nil
+	}
+
+	// aggregating all the errors here instead of having a separate one in each case.
+	if err != nil {
+		return "", false, fmt.Errorf("error processing journal entry: %w", err)
+	}
+
+	return string(dataToSend), updateCargo, err
+}
+
+func parseCargo() (string, error) {
+	cargoReadSig <- struct{}{}
+	cargoLines := <-cargoNewLinesChan
+	cargoJson := strings.Join(cargoLines, "\n")
+	return cargoJson, nil
 }
